b067a513b8de107d36ab6c348bcf324f70748d94
Halip26 <halipuddin.angko@gmail.com>
2023-06-19 01:21:40 +0800

    update all the codes

--- README.md
+++ README.md
@@ -1,14 +1,9 @@
-# Captcha Genrator 
+# Captcha Genrator
+
 A simple image captcha genrator
 
-### Prerequisites
-1. Install the dependencies by executing the following command 
-   ```pip install -r requirements.txt```
+### Changes Made
 
-2. Update the path of font in code (if required)
-    ```image = ImageCaptcha(fonts=['<path>/ChelseaMarketsr.ttf', '<path>/DejaVuSanssr.ttf'])```
-
-### Screenshot
-![image](https://user-images.githubusercontent.com/39544459/137623915-1e837ada-f199-4513-a15d-ecbb969fd53e.png)
-
-## *Mayur Singal*  
+- Redefined __random__ as a global variable, so that it can be accessed by other functions.
+- Created the __generate_image()__ function to encapsulate complex logic related to generating captcha image, which also updates __random__ global variable and makes __photo__ available globally. It's called on button click, and also on application startup to (re)display captcha image.
+- Removed redundant checks of __isinstance(data, BytesIO)__ on each __generate()__ call.
--- main.py
+++ main.py
@@ -1,54 +1,46 @@
-"""
-Instruksi for student :
-1. Instal captcha: pip instal captcha
-2. unduh font dan perbarui jalur dalam kode
-3. jalankan kode
-
-"""
-
 from io import BytesIO
 from tkinter import *
-from random import *
+from random import randint
 from tkinter import messagebox
-import string
 from captcha.image import ImageCaptcha
 
-image = ImageCaptcha(fonts=['./fonts/ChelseaMarketsr.ttf', './fonts//DejaVuSanssr.ttf'])
+image = ImageCaptcha(
+    fonts=['./fonts/ChelseaMarketsr.ttf', './fonts/DejaVuSanssr.ttf'])
 
-random=str(randint(100000,999999))
-data = image.generate(random)
-assert isinstance(data, BytesIO)
-image.write(random,'out.png')
+
+def generate_image():
+    global random, photo
+    random = str(randint(100000, 999999))
+    data = image.generate(random)
+    assert isinstance(data, BytesIO)
+    image.write(random, './captcha_image/output.png')
+    photo = PhotoImage(file='./captcha_image/output.png')
+    l1.config(image=photo, height=100, width=200)
+
 
 def verify():
-    global random
-    x=t1.get("0.0",END)
-    if (int(x)==int(random)):
+    x = t1.get("0.0", END).strip()
+    if (int(x) == int(random)):
         messagebox.showinfo("sucsess", "verified")
+        generate_image()
     else:
         messagebox.showinfo("Alert", "Not verified")
-        refresh()
+        generate_image()
 
-def refresh():
-        random=str(randint(100000,999999))
-        data = image.generate(random)
-        assert isinstance(data, BytesIO)
-        image.write(random,'out.png')
-        photo = PhotoImage(file="out.png")
-        l1.config(image=photo,height=100,width=200)
-        l1.update()
-        UpdateLabel()
-    
-root=Tk()
-photo = PhotoImage(file="out.png")
 
-l1=Label(root,image=photo,height=100,width=200)
-t1=Text(root,height=5,width=50)
-b1=Button(root,text="submit",command=verify)
-b2=Button(root,text="refresh",command=refresh)
+root = Tk()
+photo = None  # To avoid UnboundLocalError
+
+l1 = Label(root, height=100, width=200)
+t1 = Text(root, height=5, width=50)
+b1 = Button(root, text="submit", command=verify)
+b2 = Button(root, text="refresh", command=generate_image)
 
 l1.pack()
 t1.pack()
 b1.pack()
 b2.pack()
+
+generate_image()  # To display image on application startup
+
 root.mainloop()
